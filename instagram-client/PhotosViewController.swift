8341e3cd8d97ad47dc05c6df721f6b89777b4bfc
gideong <gideong@yahoo-inc.com>
2015-08-26 16:32:23 -0700

    WIP

--- instagram-client/PhotosViewController.swift
+++ instagram-client/PhotosViewController.swift
@@ -13,7 +13,39 @@ class PhotosViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let clientId = "b3f395b259d3483b9e708b58adff634d"
+        let url = NSURL(string: "https://api.instagram.com/v1/media/popular?client_id=\(clientId)")!
+        let request = NSURLRequest(URL: url)
+
+        // TODO: try to get this working with NSURLSession instead?
+//        let session = NSURLSession.sharedSession()
+//        session.dataTaskWithRequest(request, completionHandler: {(data, response, error) in
+//            print(data)
+//            print(response)
+//            print(error)
+//        })?.resume()
+        
+        
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response: NSURLResponse?, data: NSData?, error: NSError?) -> Void in
+            if (error != nil) {
+                print(error)
+                return
+            } else {
+                print("ok")
+            }
+//            do {
+//                let gdata = data as NSData!
+//                let json = try NSJSONSerialization.JSONObjectWithData(gdata, options: NSJSONReadingOptions.AllowFragments) as? NSDictionary
+//                if let json = json {
+//                        print(json)
+//                }
+//            } catch {
+//                print("deserialize error")
+//            }
+//            self.photos = responseDictionary["data"] as! NSArray
+//            self.tableView.reloadData()
+//            print(self.photos)
+        }
     }
 
     override func didReceiveMemoryWarning() {
