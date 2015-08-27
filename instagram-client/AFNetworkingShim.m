a399518e40d08459d44aa420b6c7a811fde2c7dc
gideong <gideong@yahoo-inc.com>
2015-08-26 17:11:32 -0700

    bridge file n' shit

---
+++ instagram-client/AFNetworkingShim.m
@@ -0,0 +1,10 @@
+//
+//  AFNetworkingShim.m
+//  instagram-client
+//
+//  Created by Gideon Goodwin on 8/26/15.
+//  Copyright © 2015 Gideon Goodwin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "UIImageView+AFNetworking.h"
